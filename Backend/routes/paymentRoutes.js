2fba0c8cd81c40995955d25ca54c8d8cc2f3689e
douglas-rgb <douglas.ar7mdrs@gmail.com>
2026-03-25 11:31:46 -0300

    fix: mercado pago sdk

--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -1,4 +1,10 @@
+import express from "express";
+import { criarPagamento } from "../controllers/paymentController.js";
 import payment from "../config/mp.js";
+
+const router = express.Router();
+
+router.post("/pagamento", criarPagamento);
 
 router.get("/status/:id", async (req, res) => {
   try {
@@ -10,6 +16,9 @@ router.get("/status/:id", async (req, res) => {
       status: response.status,
     });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Erro ao buscar status" });
   }
 });
+
+export default router;
