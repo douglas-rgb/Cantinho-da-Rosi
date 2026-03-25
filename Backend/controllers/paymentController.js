32e08c526623a17b1dd6a946734b9ebff32d17f5
douglas-rgb <douglas.ar7mdrs@gmail.com>
2026-03-25 11:24:07 -0300

    fix: mercado pago sdk novo

--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -1,28 +1,21 @@
-import mercadopago from "../config/mp.js";
+import payment from "../config/mp.js";
 
 export const criarPagamento = async (req, res) => {
   try {
-    const { valor, produto, email } = req.body;
-
-    const payment_data = {
-      transaction_amount: Number(valor),
-      description: produto,
-      payment_method_id: "pix",
-      payer: {
-        email: email,
+    const response = await payment.create({
+      body: {
+        transaction_amount: 50,
+        description: "Produto teste",
+        payment_method_id: "pix",
+        payer: {
+          email: "teste@email.com",
+        },
       },
-    };
-
-    const response = await mercadopago.payment.create(payment_data);
-
-    console.log("PAGAMENTO CRIADO:", response.body);
-
-    res.json(response.body);
-  } catch (error) {
-    console.error("Erro no pagamento:", error.response?.data || error.message);
-
-    res.status(500).json({
-      error: "Erro ao criar pagamento",
     });
+
+    res.json(response);
+  } catch (error) {
+    console.error("Erro no pagamento:", error);
+    res.status(500).json(error);
   }
 };
--- Backend/routes/paymentRoutes.js
+++ Backend/routes/paymentRoutes.js
@@ -10,7 +10,6 @@ router.get("/status/:id", async (req, res) => {
       status: response.status,
     });
   } catch (error) {
-    console.error("Erro ao buscar status:", error);
     res.status(500).json({ error: "Erro ao buscar status" });
   }
 });
