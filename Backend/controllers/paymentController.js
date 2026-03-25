e0faf38f730c9a32728c6c21f4dd1ed84f64d614
douglas-rgb <douglas.ar7mdrs@gmail.com>
2026-03-25 11:57:17 -0300

    test: console valor

--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -1,14 +1,14 @@
-import payment from "../config/mp.js";
-
 export const criarPagamento = async (req, res) => {
   try {
+    const { valor, produto, email } = req.body;
+
     const response = await payment.create({
       body: {
-        transaction_amount: 50,
-        description: "Produto teste",
+        transaction_amount: Number(valor),
+        description: produto,
         payment_method_id: "pix",
         payer: {
-          email: "teste@email.com",
+          email: email,
         },
       },
     });
