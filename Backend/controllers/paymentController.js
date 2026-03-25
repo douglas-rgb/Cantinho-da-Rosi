0322ae0347c6e4c8d891b96fc789967766fada34
douglas-rgb <douglas.ar7mdrs@gmail.com>
2026-03-25 16:52:10 -0300

    fix: adicionando dados obrigatorios do pagador para validar pix

--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -2,18 +2,24 @@ import payment from "../config/mp.js";
 
 export const criarPagamento = async (req, res) => {
   try {
-    const { valor, produto, email } = req.body;
+   const { valor, produto, email, nome, sobrenome, cpf } = req.body;
 
-    const response = await payment.create({
-      body: {
-        transaction_amount: Number(valor),
-        description: produto,
-        payment_method_id: "pix",
-        payer: {
-          email: "test_user_123@testuser.com",
-        },
+const response = await payment.create({
+  body: {
+    transaction_amount: Number(valor),
+    description: produto,
+    payment_method_id: "pix",
+    payer: {
+      email: email, // e-mail do cliente
+      first_name: nome, // nome do cliente
+      last_name: sobrenome, // sobrenome do cliente
+      identification: {
+        type: "CPF",
+        number: cpf // CPF do cliente (apenas números)
       },
-    });
+    },
+  },
+});
 
     res.json(response);
   } catch (error) {
