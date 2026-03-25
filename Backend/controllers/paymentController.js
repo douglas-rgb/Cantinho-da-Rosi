9fc9c62dc5bf2cbf07cdc3ecdfd66f738afaa758
douglas-rgb <douglas.ar7mdrs@gmail.com>
2026-03-25 12:12:21 -0300

    fix pagamento pix

--- Backend/controllers/paymentController.js
+++ Backend/controllers/paymentController.js
@@ -1,21 +1,29 @@
+import mercadopago from "../config/mp.js";
+
 export const criarPagamento = async (req, res) => {
   try {
     const { valor, produto, email } = req.body;
 
-    const response = await payment.create({
-      body: {
-        transaction_amount: Number(valor),
-        description: produto,
-        payment_method_id: "pix",
-        payer: {
-          email: email,
-        },
-      },
-    });
+    console.log("VALOR:", valor);
+    console.log("PRODUTO:", produto);
+    console.log("EMAIL:", email);
 
-    res.json(response);
+    const payment_data = {
+      transaction_amount: Number(valor),
+      description: produto,
+      payment_method_id: "pix",
+      payer: {
+        email: email,
+      },
+    };
+
+    const response = await mercadopago.payment.create(payment_data);
+
+    console.log("RESPOSTA MP:", response.body);
+
+    res.json(response.body);
   } catch (error) {
-    console.error("Erro no pagamento:", error);
-    res.status(500).json(error);
+    console.error("Erro no pagamento:", error.message);
+    res.status(500).json({ error: "Erro ao gerar pagamento" });
   }
 };
